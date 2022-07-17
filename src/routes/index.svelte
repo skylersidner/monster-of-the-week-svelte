269c264da8a62a98129b57bee9a7f9b741712294
skylersidner <malevolentdragon@gmail.com>
2022-07-17 18:27:46 -0500

    Adding TailwindCSS and first pass at layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex-1 flex items-stretch overflow-hidden">
+	<main class="flex-1 overflow-y-auto">
+		<!-- Primary column -->
+		<section
+			aria-labelledby="primary-heading"
+			class="min-w-0 flex-1 h-full flex flex-col lg:order-last"
+		>
+			<h1 id="primary-heading" class="sr-only">Photos</h1>
+			<!-- Your content -->
+		</section>
+	</main>
+
+	<!-- Secondary column (hidden on smaller screens) -->
+	<aside class="hidden w-96 bg-white border-l border-gray-200 overflow-y-auto lg:block">
+		<!-- Your content -->
+	</aside>
+</div>
